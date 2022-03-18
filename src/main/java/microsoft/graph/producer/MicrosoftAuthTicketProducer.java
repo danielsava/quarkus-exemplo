1067fbf58bacff973ab44047e47fbe70013afed0
DANIEL_S <daniel.sava@tcm.go.gov.br>
2022-03-18 18:20:13 -0300

    MicrosoftGraph: testes

--- src/main/java/microsoft/graph/producer/MicrosoftAuthTicketProducer.java
+++ src/main/java/microsoft/graph/producer/MicrosoftAuthTicketProducer.java
@@ -2,6 +2,8 @@ package microsoft.graph.producer;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.identity.DeviceCodeCredential;
+import com.azure.identity.DeviceCodeCredentialBuilder;
 import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
 import microsoft.graph.config.MicrosoftTicketConfig;
 import microsoft.graph.producer.qualifiers.Ticket;
@@ -16,9 +18,10 @@ public class MicrosoftAuthTicketProducer {
     @Inject
     MicrosoftTicketConfig ticketConfig;
 
+    /* Credential: Secret */
     @Produces
     @Ticket
-    TokenCredentialAuthProvider tokenCredentialAuthProvider() {
+    TokenCredentialAuthProvider secretCredential() {
 
         final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
                 .clientId(ticketConfig.client())
@@ -28,6 +31,37 @@ public class MicrosoftAuthTicketProducer {
 
         return new TokenCredentialAuthProvider(ticketConfig.scopes(), clientSecretCredential);
     }
+
+    /* Credential: Username Password
+    @Produces
+    @Ticket
+    TokenCredentialAuthProvider usernamePasswordCredential() {
+        final UsernamePasswordCredential usernamePasswordCredential = new UsernamePasswordCredentialBuilder()
+                .clientId(ticketConfig.client())
+                .username("c07d2e6e-45b2-43fd-9802-1cc8b1afeca0")
+                .password("@Tcm012022")
+                .build();
+
+        return new TokenCredentialAuthProvider(ticketConfig.scopes(), usernamePasswordCredential);
+    }
+    */
+
+
+    /* Credential: Device Code
+    @Produces
+    @Ticket
+    TokenCredentialAuthProvider deviceCodeCredential() {
+
+        final DeviceCodeCredential credential = new DeviceCodeCredentialBuilder()
+                .clientId(ticketConfig.client())
+                .challengeConsumer(challenge -> System.out.println(challenge.getMessage()))
+                .build();
+
+        return new TokenCredentialAuthProvider(ticketConfig.scopes(), credential);
+    }
+
+    */
+
 
 
 }
--- src/main/java/microsoft/graph/rest/MicrosoftTicketRest.java
+++ src/main/java/microsoft/graph/rest/MicrosoftTicketRest.java
@@ -1,33 +1,46 @@
 package microsoft.graph.rest;
 
-import com.azure.identity.ClientSecretCredential;
-import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.identity.UsernamePasswordCredential;
-import com.azure.identity.UsernamePasswordCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.logger.DefaultLogger;
-import com.microsoft.graph.logger.LoggerLevel;
-import com.microsoft.graph.models.User;
+import com.microsoft.graph.models.ChatMessage;
+import com.microsoft.graph.models.ItemBody;
 import com.microsoft.graph.requests.GraphServiceClient;
-import microsoft.graph.config.MicrosoftTicketConfig;
-import microsoft.graph.model.UserInfo;
-import microsoft.graph.service.MicrosoftTicketService;
+import microsoft.graph.producer.qualifiers.Ticket;
 import okhttp3.Request;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("/microsoft/ticket")
 public class MicrosoftTicketRest {
 
 
-    @Inject
-    MicrosoftTicketService ticketService;
+    //@Inject
+    //MicrosoftTicketService ticketService;
 
-    @Inject
-    MicrosoftTicketConfig ticketConfig;
+    @Inject @Ticket
+    GraphServiceClient<Request> clientTicket;
 
+
+    @GET
+    @Path("/me")
+    public Response me() {
+
+        /* */
+        ChatMessage chatMessage = new ChatMessage();
+        ItemBody body = new ItemBody();
+        body.content = "Ticket Teste ...";
+        chatMessage.body = body;
+
+        clientTicket.teams("2362f840-9a4e-4abc-b8d7-7e4176e1eca0").channels("19%3a7df86b2319b8416a8eb20def9c3b452f%40thread.tacv2").messages()
+                .buildRequest()
+                .post(chatMessage);
+
+
+        return Response.ok().build();
+    }
+
+    /*
     @GET
     @Path("/me")
     public UserInfo teste() {
@@ -50,17 +63,24 @@ public class MicrosoftTicketRest {
         TokenCredentialAuthProvider tokenUserPassword = new TokenCredentialAuthProvider(ticketConfig.scopes(), usernamePasswordCredential);
 
 
+        final DeviceCodeCredential credential = new DeviceCodeCredentialBuilder()
+                .clientId(ticketConfig.client())
+                .challengeConsumer(challenge -> System.out.println(challenge.getMessage()))
+                .build();
+
+        TokenCredentialAuthProvider tokenDeviceCredential = new TokenCredentialAuthProvider(ticketConfig.scopes(), credential);
+
+
         DefaultLogger logger = new DefaultLogger();
         logger.setLoggingLevel(LoggerLevel.DEBUG);
 
         GraphServiceClient<Request> graphClient = GraphServiceClient
                 .builder()
-                .authenticationProvider(tokenUserPassword)
+                .authenticationProvider(tokenDeviceCredential)
                 .logger(logger)
                 .buildClient();
 
 
-        /*
         ChatMessage chatMessage = new ChatMessage();
         ItemBody body = new ItemBody();
         body.content = "Ticket Teste ...";
@@ -69,9 +89,8 @@ public class MicrosoftTicketRest {
         graphClient.teams("2362f840-9a4e-4abc-b8d7-7e4176e1eca0").channels("19%3a7df86b2319b8416a8eb20def9c3b452f%40thread.tacv2").messages()
                 .buildRequest()
                 .post(chatMessage);
-        */
 
-        /* */
+
         User user = graphClient.users("c07d2e6e-45b2-43fd-9802-1cc8b1afeca0").
                 buildRequest()
                 //.select("displayName")
@@ -81,15 +100,12 @@ public class MicrosoftTicketRest {
         userInfo.aboutMe = "Teste Ticket App";
         userInfo.displayName = user.displayName;
 
-
         return new UserInfo();
 
     }
+   */
 
-
-
-
-
+    /*
     public UserInfo infoMe() {
 
         User user = ticketService.userInfo();
@@ -100,6 +116,8 @@ public class MicrosoftTicketRest {
 
         return userInfo;
     }
+    */
+
 
 }
 
